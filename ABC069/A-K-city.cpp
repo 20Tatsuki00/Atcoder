9e8667eb32d6eaaa52e3fba483914e2031f68082
Kawauchi Tatsuki <109912025+20Tatsuki00@users.noreply.github.com>
2023-03-15 22:39:05 +0900

    Create A-K-city.cpp

---
+++ ABC069/A-K-city.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int a,b;
+    cin>>a>>b;
+    
+    cout<<(a-1)*(b-1);
+    
+    return 0;
+}
