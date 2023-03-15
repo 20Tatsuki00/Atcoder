9f86796187c7a6824dbf1a5ffaf9ca2008b42ee3
Kawauchi Tatsuki <109912025+20Tatsuki00@users.noreply.github.com>
2023-03-15 22:53:41 +0900

    Create A - Sandglass2.cpp

---
+++ ABC072/A - Sandglass2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int x,t;
+    cin>>x>>t;
+    if(x>t){
+        cout<<x-t;
+    }else cout<<0;
+    return 0;
+}
