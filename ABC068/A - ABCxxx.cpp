b0a83d274c03b53d2cbf6d043a1f5eae7e6dfc26
Kawauchi Tatsuki <109912025+20Tatsuki00@users.noreply.github.com>
2023-03-15 22:43:38 +0900

    Create A - ABCxxx.cpp

---
+++ ABC068/A - ABCxxx.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+   
+    string s;
+    cin>>s;
+    
+    cout<<"ABC"<<s;
+    return 0;
+}
